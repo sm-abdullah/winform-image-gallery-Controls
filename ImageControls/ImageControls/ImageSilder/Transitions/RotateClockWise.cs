d7d1d236cc21b3a311f45e8dabde6d704b16ecba
sm abdullah <sm.assm@gmail.com>
2017-07-30 01:49:19 +0700

    fixed rotate clock wise

--- ImageControls/ImageControls/ImageSilder/Transitions/RotateClockWise.cs
+++ ImageControls/ImageControls/ImageSilder/Transitions/RotateClockWise.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
@@ -16,7 +17,7 @@ namespace ImageControls.ImageSilder.Transitions
         private Timer timer;
         private TimeSpan speed = TimeSpan.FromSeconds(3);
         private DateTime startTime = DateTime.MinValue;
-        private float angle = 0;
+        
         private float size = 1;
         public RotateClockWise()
         {
@@ -29,18 +30,29 @@ namespace ImageControls.ImageSilder.Transitions
             Matrix m = new Matrix();
             var height =  (foregroudnImage.Height / 10) * size;
             var width = (foregroudnImage.Width / 10) * size;
-            size += 0.05f;
+            size += 0.1f;
+
+            var percentage = (size / 10) * 100;
+            float angle = (720 / 100) * percentage;
+            if (angle > 720)
+            {
+                angle = 720;
+            }
             
             var rec = new Rectangle((clientRectangle.Width / 2) - (int)(width/2),(clientRectangle.Height / 2) -(int)(height/2), (int) width, (int)height);
             m.RotateAt(angle, new PointF(clientRectangle.Left + (clientRectangle.Width / 2), clientRectangle.Top + (clientRectangle.Height / 2) ));
             graphics.DrawImage(backgroundImage, clientRectangle);
             graphics.Transform = m;
-          
+
+            var v = angle % 180;
+            Debug.WriteLine(angle + "-----------" + v);
             graphics.DrawImage(foregroudnImage, rec);
-            if (rec.Height > foregroudnImage.Height && rec.Width > foregroudnImage.Width)
+            if (size >= 10 &&  angle >= 720)
             {
                 if (Finished != null)
                 {
+                    
+                    timer.Stop();
                     Finished.Invoke(this, EventArgs.Empty);
                 }
             }
@@ -48,7 +60,7 @@ namespace ImageControls.ImageSilder.Transitions
         void timer_Elapsed(object sender, ElapsedEventArgs e)
         {
 
-            angle++;
+          
             if (Changed != null)
             {
                 Changed.Invoke(this, EventArgs.Empty);
@@ -59,7 +71,7 @@ namespace ImageControls.ImageSilder.Transitions
         public void Start()
         {
             startTime = DateTime.Now;
-            timer.Interval = 2;
+            timer.Interval = 0.1;
             timer.Start();
         }
     }
